21a36468b602c0329ecddbfa6aec5c9c05a73deb
JacobLRL <JLittle@spartaglobal.com>
2019-01-16 10:02:21 +0000

    added feature to use build car

--- js/app.js
+++ js/app.js
@@ -72,11 +72,12 @@ class Garage {
         this.country = country;
         this.city = city;
         this.capacity = capacity;
-        this.cars = createCars(capacity);
+        this.cars = createCars(capacity - 1);
     }
 
     buildCar(make, model, color) {
         const newCar = new Car(make, model, color, 2019);
+        this.cars.push(newCar);
     }
 }
 
@@ -89,9 +90,18 @@ country = prompt("I seem to have just woken up and forgotten where I am.\n What 
 city = prompt("And what city are we in?");
 alert(`Welcome to my garage in ${city}, ${country}`);
 capacity = prompt("How many cars do you think there are in my garage?");
-alert(`Great guess! There are infact ${capacity} in my garage`);
+alert(`Great guess! There are infact ${capacity - 1} in my garage with space to build one more`);
 
 const myGarage = new Garage(country, city, capacity);
 
 console.log(myGarage);
 
+let yesNo = prompt("Would you like to build a new car? 'yes' or 'no'");
+if (yesNo === "yes") {
+    let make = prompt("what make would you like?");
+    let model = prompt("what model would you like?");
+    let color  = prompt("what colour would you like?")
+    myGarage.buildCar(make, model, color);
+}
+
+console.log(myGarage);
